7858df3902aaf6ca4c28c72d2e93baf1deaa5e7f
Chibex <triumphanazia@gmail.com>
2019-07-08 21:57:10 +0100

    Semester 2 Official Project

--- src/checkbook/withdrawal.java
+++ src/checkbook/withdrawal.java
@@ -1,14 +1,21 @@
 package checkbook;
 
+import static checkbook.deposit.Total;
+
+import static checkbook.deposit.count;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class withdrawal {
     deposit obj = new deposit();
+    accountSummary obj2 = new accountSummary();
     Scanner input = new Scanner(System.in);
     
     public void withdraw(){
         Checkbook objcheck = new Checkbook();
-        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now();
 //        This block checks if your balance is less or equal zero
         if(obj.Total <= 0){
             System.out.println("Sorry! You cannot make any withdraws at the moment");
@@ -24,7 +31,10 @@ public class withdrawal {
         if(obj.Total >= amnt){
             obj.Total -= amnt;
             System.out.println("Withdraw Complete! "+java.time.LocalDateTime.now());
+            String theTime = dtf.format(now);
+            accountSummary.account[count] = "Transaction [Withdrawal] USD "+ Double.toString(Total)+" Time: "+theTime;
             System.out.println("Your New Balance is: USD "+obj.Total);
+            count++;
             objcheck.mainActivity();
         }else{
             System.out.println("Insufficient Funds!");
