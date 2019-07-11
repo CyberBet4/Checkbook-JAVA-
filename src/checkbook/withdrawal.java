dd052b8a6f99297aa8b2cebf99f01150b1c2972b
Chibex <triumphanazia@gmail.com>
2019-07-11 16:19:53 +0100

    Semester 2 Official Project

--- src/checkbook/withdrawal.java
+++ src/checkbook/withdrawal.java
@@ -10,30 +10,37 @@ public class withdrawal {
     accountSummary obj2 = new accountSummary();
     Scanner input = new Scanner(System.in);
     
+    public void clrscr(){
+        System.out.print("\033[H\033[2J");
+        System.out.println("\b\b\b");
+        System.out.flush();
+    }
     public void withdraw(){
         Checkbook objcheck = new Checkbook();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
         LocalDateTime now = LocalDateTime.now();
 //        This block checks if your balance is less or equal zero
-        if(obj.Total <= 0){
+        if(deposit.Total <= 0){
             System.out.println("Sorry! You cannot make any withdraws at the moment");
-            System.out.println("CURRENT BALANCE: USD "+obj.Total);
+            System.out.println("CURRENT BALANCE: USD "+deposit.Total);
             objcheck.mainActivity();
         }else{
         System.out.println("Enter amount to withdraw");
         System.out.print("USD ");
         double amnt = input.nextDouble();
         
-//        this block checks if your current balance is less or greater than your
-//            withdraw amount and prompts error if the withdraw is more than
-//            the current balance.
-        if(obj.Total >= amnt){
-            obj.Total -= amnt;
+/**        this block checks if your current balance is less or greater than your
+*           withdraw amount and prompts error if the withdraw is more than
+*            the current balance.
+*/        
+        if(deposit.Total >= amnt){
+            deposit.Total -= amnt;
 //            Clear screen function
-            System.out.println("Withdraw Complete! "+java.time.LocalDateTime.now());
+                clrscr();
+            System.out.println("Withdraw Complete! "+dtf.format(now));
             String theTime = dtf.format(now);
             accountSummary.account[count] = "Transaction [Withdrawal] USD "+ Double.toString(amnt)+" Time: "+theTime;
-            System.out.println("Your New Balance is: USD "+obj.Total);
+            System.out.println("Your New Balance is: USD "+Checkbook.round(deposit.Total, 2));
             count++;
             objcheck.mainActivity();
         }else{
